1c218909fe62f10db31872f239373eddc3871df9
Nithze <aldiyusronzhar@gmail.com>
2024-02-20 15:55:46 +0700

    add form edit and add stying heart ahh

--- frontend/src/components/pages/UserManage.vue
+++ frontend/src/components/pages/UserManage.vue
@@ -1,12 +1,37 @@
 <script setup>
 // import { ref } from 'vue'
 import Navbar from "../utils/Navbar.vue";
+import EditForm from "../utils/EditForm.vue";
+import AddForm from "../utils/AddForm.vue";
+import { ref } from "vue";
+
+const showEditForm = ref(false);
+const showEditFormModal = () => {
+  showEditForm.value = true;
+};
+const closeFormModal = () => {
+  showEditForm.value = false;
+};
+
+
+const showAddForm = ref(false);
+const showAddFormModal = () => {
+  showAddForm.value = true;
+};
+const closeAddModal = () => {
+  showAddForm.value = false;
+};
+
 </script>
 
 <template>
+  <div v-if="showAddForm" class="overlay"></div>
+  <div v-if="showEditForm" class="overlay"></div>
+  <EditForm v-if="showEditForm" @close-form-modal="closeFormModal" :class="{ 'fade-scale-in': showEditForm }" />
+  <AddForm v-if="showAddForm" @close-add-modal="closeAddModal" :class="{ 'fade-scale-in': showAddForm }" />
   <div class="container">
     <Navbar />
-
+    <button class="add-button" @click="showAddFormModal">Add User</button>
     <div class="data-user-container">
       <table>
         <thead>
@@ -25,7 +50,7 @@ import Navbar from "../utils/Navbar.vue";
             <td>123456789</td>
             <td>User</td>
             <td class="action-button">
-              <button>Edit</button>
+              <button @click="showEditFormModal">Edit</button>
               <button>Delete</button>
             </td>
           </tr>
@@ -73,7 +98,7 @@ table {
 
 th,
 td {
-  border: none; 
+  border: none;
   /* text-align: left; */
   padding: 8px;
   color: var(--brand-dark);
@@ -85,7 +110,7 @@ th {
 }
 
 tbody tr:nth-child(even) {
-  background-color: #f9f9f9; 
+  background-color: #f9f9f9;
 }
 
 .action-button {
@@ -107,13 +132,49 @@ tbody tr:nth-child(even) {
   margin: 2px 2px;
   cursor: pointer;
   border-radius: 3px;
-  transition: background-color 0.3s ease; 
+  transition: background-color 0.3s ease;
 }
 
 .action-button button:hover {
   background-color: var(--brand-primary-hover);
 }
 
+.add-button {
+  width: 100px;
+  margin-left: 87%;
+  background-color: var(--brand-primary);
+  border: none;
+  border-radius: 3px;
+  padding: 5px 10px
+}
+
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 100;
+}
+
+
+.fade-scale-in {
+  animation: fadeScaleIn 0.4s;
+}
+
+@keyframes fadeScaleIn {
+  from {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
 
 </style>
 
